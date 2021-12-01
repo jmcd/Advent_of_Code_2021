cb21727c78250e6e45da5ff2a8f447f4a3150d4b
John McDowall <jmcd@users.noreply.github.com>
2021-12-01 16:26:10 +0000

    Add day 1

---
+++ AoC/Day01.cs
@@ -0,0 +1,81 @@
+namespace AoC;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+public class Day01
+{
+    private readonly ITestOutputHelper _output;
+
+    public Day01(ITestOutputHelper output) => _output = output;
+
+    [InlineData("day01_example.txt")]
+    [InlineData("day01.txt")]
+    [Theory]
+    public async Task Part1(string filename)
+    {
+        var depths = await LoadDepthsAsync(filename);
+
+        var count = CountOfValueGreaterThanPrevious(depths);
+
+        _output.WriteLine($"{count}");
+    }
+
+    [InlineData("day01_example.txt")]
+    [InlineData("day01.txt")]
+    [Theory]
+    public async Task Part2(string filename)
+    {
+        var depths = await LoadDepthsAsync(filename);
+
+        var windows = MakeSlidingWindows(3, depths);
+
+        var count = CountOfValueGreaterThanPrevious(windows);
+
+        _output.WriteLine($"{count}");
+    }
+
+    private static async Task<IEnumerable<int>> LoadDepthsAsync(string filename) => (await File.ReadAllLinesAsync(Path.Combine("input", filename))).Select(int.Parse);
+
+    private static int CountOfValueGreaterThanPrevious(IEnumerable<int> depths)
+    {
+        var count = 0;
+
+        var prevDepth = default(int?);
+
+        foreach (var depth in depths)
+        {
+            if (depth > prevDepth)
+            {
+                count += 1;
+            }
+
+            prevDepth = depth;
+        }
+
+        return count;
+    }
+
+    private IEnumerable<int> MakeSlidingWindows(int windowLength, IEnumerable<int> values)
+    {
+        var window = new List<int>();
+
+        foreach (var depth in values)
+        {
+            if (window.Count == windowLength)
+            {
+                window.RemoveAt(0);
+            }
+
+            window.Add(depth);
+            if (window.Count == windowLength)
+            {
+                yield return window.Sum();
+            }
+        }
+    }
+}
