e0ad505d2710291c72f09ec745236771170029e5
John McDowall <jmcd@users.noreply.github.com>
2021-12-12 11:50:21 +0000

    Add day 12 part 2

--- AoC/Day12.cs
+++ AoC/Day12.cs
@@ -2,16 +2,12 @@ namespace AoC;
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
-using Xunit.Abstractions;
 
 public class Day12
 {
-    private ITestOutputHelper _op;
-    public Day12(ITestOutputHelper op) => _op = op;
-
     [Theory]
     [InlineData("day12_example1.txt", 10)]
     [InlineData("day12_example2.txt", 19)]
@@ -19,50 +15,105 @@ public class Day12
     [InlineData("day12.txt", 4691)]
     public async void Part1(string filename, int expectation)
     {
-        Debug.WriteLine(filename, expectation);
+        var connections = await ReadInputConnections(filename);
+        var paths = FindDistinctPaths(connections);
+        Assert.Equal(expectation, paths.Count);
+    }
 
-        var lines = await Input.ReadAllLinesAsync(filename);
-
-        var allConnections = lines.Select(line =>
-        {
-            var caveNames = line.Split("-");
-            return new Connection(caveNames[0], caveNames[1]);
-        }).ToArray();
-
-        var paths = FindDistinctPaths(allConnections);
-
+    [Theory]
+    [InlineData("day12_example1.txt", 36)]
+    [InlineData("day12_example2.txt", 103)]
+    [InlineData("day12_example3.txt", 3509)]
+    [InlineData("day12.txt", 140718)]
+    public async void Part2(string filename, int expectation)
+    {
+        var connections = await ReadInputConnections(filename);
+        var paths = FindDistinctPaths_AllowingForVisitingSingleSmallCaveTwice(connections);
         Assert.Equal(expectation, paths.Count);
     }
 
     private static List<string[]> FindDistinctPaths(Connection[] allConnections)
     {
+        void Recurse(string[] path, ICollection<string[]> completedPaths)
+        {
+            var caveName = path[^1];
+            if (caveName == "end")
+            {
+                completedPaths.Add(path);
+                return;
+            }
+
+            var unvisitableCaves = path.Where(vc => char.IsLower(vc[0]));
+
+            var exitCaves = allConnections
+                .Where(p => p.HasCave(caveName))
+                .Select(p => p.OtherCave(caveName))
+                .Except(unvisitableCaves);
+
+            foreach (var exitCave in exitCaves)
+            {
+                var continuedPath = path.Append(exitCave).ToArray();
+                Recurse(continuedPath, completedPaths);
+            }
+        }
+
         var completedPaths = new List<string[]>();
-        FindDistinctPaths(new[] { "start" }, allConnections, completedPaths);
+        Recurse(new[] { "start" }, completedPaths);
         return completedPaths;
     }
 
-    private static void FindDistinctPaths(string[] path, Connection[] allConnections, ICollection<string[]> completedPaths)
+    private static List<string[]> FindDistinctPaths_AllowingForVisitingSingleSmallCaveTwice(Connection[] allConnections)
     {
-        var caveName = path[^1];
-        if (caveName == "end")
+        void Recurse(string[] path, ISet<string[]> completedPaths)
         {
-            completedPaths.Add(path);
-            return;
+            var caveName = path[^ 1];
+            if (caveName == "end")
+            {
+                completedPaths.Add(path);
+                return;
+            }
+
+            var exitCaves = allConnections
+                .Where(p => p.HasCave(caveName))
+                .Select(p => p.OtherCave(caveName))
+                .ToArray();
+
+            var exitSmallCaves = exitCaves.Where(ec => ec != "start" && char.IsLower(ec[0])).ToArray();
+
+            var visitedSmallCaves = path.Where(vc => char.IsLower(vc[0])).ToArray();
+
+            exitCaves = exitCaves.Except(visitedSmallCaves).ToArray();
+
+            var canVisitASmallCave = visitedSmallCaves.Aggregate(new Dictionary<string, int>(), (caveToCount, cave) =>
+            {
+                if (!caveToCount.TryGetValue(cave, out var currentCount)) { currentCount = 0; }
+                caveToCount[cave] = currentCount + 1;
+                return caveToCount;
+            }).All(kvp => kvp.Value == 1);
+
+            if (canVisitASmallCave)
+            {
+                exitCaves = exitCaves.Union(exitSmallCaves).ToArray();
+            }
+
+            foreach (var exitCave in exitCaves)
+            {
+                var continuedPath = path.Append(exitCave).ToArray();
+                Recurse(continuedPath, completedPaths);
+            }
         }
 
-        var unvisitableCaves = path.Where(vc => char.IsLower(vc[0]));
-
-        var exitCaves = allConnections
-            .Where(p => p.HasCave(caveName))
-            .Select(p => p.OtherCave(caveName))
-            .Except(unvisitableCaves);
-
-        foreach (var exitCave in exitCaves)
-        {
-            var continuedPath = path.Append(exitCave).ToArray();
-            FindDistinctPaths(continuedPath, allConnections, completedPaths);
-        }
+        var completedPaths = new HashSet<string[]>();
+        Recurse(new[] { "start" }, completedPaths);
+        return completedPaths.ToList();
     }
+
+    private static async Task<Connection[]> ReadInputConnections(string filename) =>
+        (await Input.ReadAllLinesAsync(filename)).Select(line =>
+        {
+            var caveNames = line.Split("-");
+            return new Connection(caveNames[0], caveNames[1]);
+        }).ToArray();
 
     private readonly record struct Connection(string LeftCave, string RightCave)
     {
